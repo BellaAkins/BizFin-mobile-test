806ee518a73718c741e17a8b0121a39f05d7a854
Sarabella <110487056+BellaAkins@users.noreply.github.com>
2024-05-10 03:03:59 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+const menu = document.querySelector(".menu");
+const close = document.querySelector(".close");
+const nav = document.querySelector("nav");
+
+menu.addEventListener("click", () => {
+  nav.classList.add("open-nav");
+});
+
+close.addEventListener("click", () => {
+  nav.classList.remove("open-nav");
+});
+
+const carousel = document.querySelector(".testimonial-box");
+
+let isDragging = false,
+  startX,
+  startScrollLeft;
+
+const dragStart = (e) => {
+  isDragging = true;
+  carousel.classList.add("dragging");
+  // Records the initial cursor and scroll position of the carousel
+  startX = e.pageX;
+  startScrollLeft = carousel.scrollLeft;
+};
+const dragging = (e) => {
+  if (!isDragging) return; // if isDragging is false return from here
+  // Updates the scroll position of the carousel based on the cursor movement
+  carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+};
+
+const dragStop = () => {
+  isDragging = false;
+  carousel.classList.remove("dragging");
+};
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+document.addEventListener("mouseup", dragStop);
